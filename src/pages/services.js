dc82219079adccd608de1129e7df124a93bacc06
Ashwin S <aswinjeganath@gmail.com>
2019-02-17 11:23:51 +0530

    round some corners

--- src/pages/services.js
+++ src/pages/services.js
@@ -1,9 +1,5 @@
 import React from 'react'
-import {
-  Container,
-  Row,
-  Col
-} from 'reactstrap'
+import { Container, Row, Col } from 'reactstrap'
 
 import styled from 'styled-components'
 
@@ -11,91 +7,75 @@ import Layout from '../components/layout'
 import SEO from '../components/seo'
 
 const ServiceList = styled.div`
-  background-color: rgba(255,255, 255, 0.85);
-  max-width: ${960/16}rem;
+  background-color: rgba(255, 255, 255, 1);
+  border-radius: 10px;
+  max-width: ${960 / 16}rem;
 `
 
 const data = [
   {
     title: 'Career Counseling',
     items: [
-      "Specialization",
-      "Scores",
-      "Right time to apply",
-      "Expenditure",
-      "University selection",
-    ]
+      'Specialization',
+      'Scores',
+      'Right time to apply',
+      'Expenditure',
+      'University selection',
+    ],
   },
   {
-    title: "Guidance for English Test",
+    title: 'Guidance for English Test',
+    items: ['Pearson Test of English (PTE)', 'IELTS', 'TOEFL'],
+  },
+  {
+    title: 'Applicant Screening',
+    items: ['Academic Excellence', 'Behavioral Excellence'],
+  },
+  {
+    title: 'Online Application',
     items: [
-      "Pearson Test of English (PTE)",
-      "IELTS",
-      "TOEFL",
-    ]
+      'Filing',
+      'University Application form',
+      'Departmental Form',
+      'Scholarship Forms',
+      'Additional Forms',
+    ],
   },
   {
-    title: "Applicant Screening",
+    title: 'Tracking',
+    items: ['Contact International Education Department'],
+  },
+  {
+    title: 'Complete Visa Guidance',
     items: [
-      "Academic Excellence",
-      "Behavioral Excellence",
-    ]
+      'Visa Fee',
+      'Legal Check- Police Verification Certificate',
+      'Visa Form filing',
+      'Visa Appointment',
+      'Guidance of Visa documentation ',
+    ],
   },
   {
-    title: "Online Application",
-    items: [
-      "Filing",
-      "University Application form",
-      "Departmental Form",
-      "Scholarship Forms",
-      "Additional Forms",
-    ]
+    title: 'Post Visa Guidance',
+    items: ['Flight Ticket Booking', 'Accommodation'],
   },
-  {
-    title: "Tracking",
-    items:[
-      "Contact International Education Department"
-    ]
-  },
-  {
-    title: "Complete Visa Guidance",
-    items: [
-      "Visa Fee",
-      "Legal Check- Police Verification Certificate",
-      "Visa Form filing",
-      "Visa Appointment",
-      "Guidance of Visa documentation ",
-    ]
-  },
-  {
-    title: "Post Visa Guidance",
-    items: [
-      "Flight Ticket Booking",
-      "Accommodation",
-    ]
-  }
 ]
-
 
 const Services = () => (
   <Layout>
     <SEO title="Services" />
     <Container className="py-3">
       <h3 className="py-4 display-4 text-center">Services</h3>
-      <ServiceList className="shadow p-5 mb-5 mx-auto">
+      <ServiceList className="shadow-lg p-5 mb-5 mx-auto">
         {data.map(service => (
           <Row key={service.title} className="py-4">
             <Col sm={4}>
-              <h4 className="font-weight-light">
-                {service.title}
-              </h4>
+              <h4 className="font-weight-light">{service.title}</h4>
             </Col>
             <Col sm={8}>
               <ul>
                 {service.items.map((item, i) => (
-                  <li key={i}>
-                    {item}
-                  </li>
+                  <li key={i}>{item}</li>
                 ))}
               </ul>
             </Col>
